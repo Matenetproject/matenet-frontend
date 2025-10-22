c263cfc036ab7dd612ff69676c5f82f37a6f0cbd
You Song Hou <ysongweb3@gmail.com>
2025-10-22 15:52:07 -0400

    chore: install react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { HashRouter, Route, Routes } from 'react-router-dom';
+
 import QRCodeReader from './pages/QRCodeReader';
 import NFCReader from './pages/NFCReader';
+import Home from './pages/Home';
 
 export default function App() {
 
   return (
-    <div>
-      <QRCodeReader />
-      <NFCReader />
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route
+          path="/scannfc"
+          element={<NFCReader />} />
+        <Route
+          path="/scanqr"
+          element={<QRCodeReader />} />
+        <Route
+          path="/"
+          element={<Home />} />
+      </Routes>
+    </HashRouter>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <Link to="/scannfc">
+        Scan NFC
+      </Link>
+      <Link to="/scanqr">
+        Scan QR
+      </Link>
+    </div>
+  )
+}
+
+export default Home
